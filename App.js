5cf05fb94853b8461e44e90b1059a5896a3bace3
Ajmal khan <144774577+ajmalkhangithub@users.noreply.github.com>
2024-08-03 18:51:33 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      <h1 className="heading_style" style={{ textAlign: "center" }}>{count}</h1>
+      <button onClick={incrementCount}>Click me!</button>
+    </>
+  );
+}
+
+export default App;
